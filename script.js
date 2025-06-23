e37b7c71db42bf58a6b175a233cca29f109f590b
leticia26022007 <145727400+leticia26022007@users.noreply.github.com>
2025-06-23 08:36:27 -0300

    mudei coigo denovo

--- script.js
+++ script.js
@@ -60,15 +60,3 @@ function mostraAlternativas() {
   }
 }
 mostraPergunta();
-botao.addEventListener("click", () => respostaSelecionada(opcao));
-function respostaSelecionada(opcaoSelecionada) {
-  const afirmacoes = opcaoSelecionada.afirmacoes;
-  atual++;
-  mostraPergunta();
-}
-function respostaSelecionada(opcaoSelecionada) {
-  const afirmacoes = opcaoSelecionada.afirmacoes;
-  historiaFinal += afirmacoes + " ";
-  atual++;
-  mostraPergunta();
-}
